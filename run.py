7f4c85a5e46313139f1a914c663372fd0f786690
Kristine Kamola <kristine.kamola@gmail.com>
2024-09-26 11:11:36 +0000

    add while loop for guess the word and remaining tries

--- run.py
+++ run.py
@@ -139,8 +139,45 @@ def select_level():
     elif choice == 2:
         return 8
     else:
-        return 
-        
+        return 6
+
+def play_game(word, max_tries, hint):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = max_tries
+    score = 0
+    hints = 0
+
+    print("Welcome to Hangman!\n")
+    print(f"You have {tries} attempts to guess the word.")
+    print (display_hangman(tries))
+    print(word_completion)
+    print("\n")
+
+    while not guessed and tries > 0:
+        guess = input("Please guess a letter or word:").lower()
+
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print(f"You already guessed the letter '{guess}'.")
+            elif guess not in word:
+                print(f"'{guess}' is not in the word.")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print(f"Good job! '{guess}'is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                score += 10
+
+                if "_" not in word_completion:
+                    guessed = True    
 
 
         
