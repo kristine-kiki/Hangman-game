48263d371e1487a67a7beb141cdc673f0e07a5f8
Kristine Kamola <kristine.kamola@gmail.com>
2024-09-28 18:08:54 +0000

    fixing bugs

--- run.py
+++ run.py
@@ -1,29 +1,28 @@
 import random
 
-#divisions
+# divisions to choose from
 
 divisions = {
 
     "Flowers": ['daisy', 'daffodil', 'hibiscus', 'lavander', 'primrose',
-     'dandelion', 'calendula', 'geranium', 'petunia', 'snowdrop'],
+                'dandelion', 'calendula', 'geranium', 'petunia', 'snowdrop'],
     "Trees": ['birch', 'oak', 'larch', 'willow', 'maple', 'chesnut tree',
-     'spruce', 'magnolia', 'cypress', 'juniper'],
-    "Birds": ['stork', 'peacock', 'hummingbird', 'pigeon', 'penguin', 
-    'woodpecker', 'swan', 'parrot', 'robin', 'blackbird']
+              'spruce', 'magnolia', 'cypress', 'juniper'],
+    "Birds": ['stork', 'peacock', 'hummingbird', 'pigeon', 'penguin',
+              'woodpecker', 'swan', 'parrot', 'robin', 'blackbird']
 }
-#hints for each word
+# hints for each word
 hints = {
 
-    "daisy": "'He loves me, he loves me not' a familiar rhyme associated with ",
-    "daffodil": "Other name of Narcissus-one of the most popular flowers in the world, mainly due to the well-known myth of the Greek youth who fell in love with himself",
+    "daisy": "'He loves me, he loves me not' a familiar rhyme associated with",
     "hibiscus": "This flower is the floral emblem of Haiti and Malaysia",
     "lavander": "Originally from the Latin word that means 'to wash'",
-    "primrose": "In the United Kingdom, it is against the law to pick this flower growing in the wild",
-    "dandelion": "This flower`s seeds can travel up to five kilometers from their original location",
     "calendula": "This flower is also known as the 'flower of the rains'",
-    "geranium": "The essential oils in certain types of scented this flower help alleviate depression and stress",
     "petunia": "This flower is related to tobacco",
+    "primrose": "In the United Kingdom, it is against the law to pick this flower growing in the wild",
     "snowdrop": "They were named after earrings",
+    "geranium": "The essential oils in certain types of scented this flower help alleviate depression and stress",
+    "dandelion": "This flowers seeds can travel up to five kilometers from their original location",
     "birch": "The most common tree all over the world",
     "oak": "These trees appeared on our planet about 65 million years ago",
     "larch": "Wood from this tree is very valuable because it is resistant and it grows quickly",
@@ -36,7 +35,7 @@ hints = {
     "juniper": "For more than 300 years, berries of this tree have been a popular flavoring agent for gin",
     "stork": "They are symbols of good luck and prosperity",
     "peacock": "This bird take three years to develop its tail plumage",
-    "hummingbird": "Their wings beat about 70 times per second (200 times per second when diving!)",
+    "hummingbird": "Their wings beat about 70 times per second(200 times per",
     "pigeon": "This bird is renowned for its outstanding navigational abilities",
     "penguin": "They spend nearly 50 prcent of their lives in the water and the other 50 procent on land",
     "woodpecker": "Called as 'a doctor of the woods'",
@@ -51,36 +50,47 @@ hints = {
 """
 Function to let the Player choose the category to play
 """
+
+
 def choose_division(divisions):
     while True:
         try:
             print("\nPlease, choose your category:")
-            for idx, division in enumerate(divisions.keys(), start=1): #display numbered categories
+            # display numbered categories
+            for idx, division in enumerate(divisions.keys(), start=1):
                 print(f"{idx}. {division}")
 
             choice = int(input("Enter the category number: "))
             if 1 <= choice <= len(divisions):
-                #get the key
-                return list(divisions.keys())[choice - 1] #return chosen category
+                # get the key and return chosen category
+                return list(divisions.keys())[choice - 1]
             else:
-                print("Invalid selection.Please Choose a valid category number.")
+                print("Incorrect!Choose a correct category number.")
         except ValueError:
-            print("Please enter valid number.") #inform player of entering invalid data 
+            # inform player of entering invalid data
+            print("Please enter valid number.")
+
 
 """
 System randomly choosing the word from chosen division
 """
+
+
 def choose_word(division):
-    words = divisions.get(division) 
+    words = divisions.get(division)
     if words:
-        return random.choice(words).lower() #choose a random word and turn it to lowercase
+        # choose a random word and turn it to lowercase
+        return random.choice(words).lower()
     else:
         print(f"No words available in the division '{division}'")
         return None
 
+
 """
 Function shows hangman based on the tries left
 """
+
+
 def display_hangman(tries, max_tries):
     stages = [
         """
@@ -96,7 +106,7 @@ def display_hangman(tries, max_tries):
            |   |
            |   O
            |  /|\\
-           |  / 
+           |  /
            |
         """,
         """
@@ -104,7 +114,7 @@ def display_hangman(tries, max_tries):
            |   |
            |   O
            |  /|\\
-           |  
+           |
            |
         """,
         """
@@ -112,7 +122,7 @@ def display_hangman(tries, max_tries):
            |   |
            |   O
            |  /|
-           |  
+           |
            |
         """,
         """
@@ -120,32 +130,35 @@ def display_hangman(tries, max_tries):
            |   |
            |   O
            |   |
-           |  
+           |
            |
         """,
         """
           ------
            |   |
            |   O
-           |   
-           |  
+           |
+           |
            |
         """,
         """
            -----
            |   |
-           |   
-           |   
-           |  
+           |
+           |
+           |
            |
         """,
-         
+
     ]
-    return stages[max_tries - tries - 1] #access stages in reveresed order
+    return stages[max_tries - tries - 1]  # access stages in reveresed order
+
 
 """
 Game logic
 """
+
+
 def play_game(word, max_tries, hint):
     if word is None:
         print("No word was selected.Exiting game.")
@@ -161,7 +174,7 @@ def play_game(word, max_tries, hint):
 
     print("\nWelcome to Hangman!")
     print(f"You have {tries} attempts to guess the word.")
-    print (display_hangman(max_tries-tries, max_tries)) #hangman state
+    print(display_hangman(max_tries-tries, max_tries))  # hangman state
     print(word_completion)
     print("\n")
 
@@ -188,20 +201,21 @@ def play_game(word, max_tries, hint):
                 guessed_letters.append(guess)
                 word_as_list = list(word_completion)
 
-                indices = [i for i, letter in enumerate(word) if letter == guess]
+                indices = [i for i, letter in enumerate(word)
+                           if letter == guess]
                 for index in indices:
-                    word_as_list[index] = guess #update word display
+                    word_as_list[index] = guess  # update word display
                 word_completion = "".join(word_as_list)
                 score += 10
 
-                if "_" not in word_completion: #check if word is complete
+                if "_" not in word_completion:  # check if word is complete
                     guessed = True
             """
             word guess
-            """    
-        elif len(guess) == len(word) and guess.isalpha(): 
+            """
+        elif len(guess) == len(word) and guess.isalpha():
             if guess in guessed_words:
-                print (f"You already guessed the word '{guess}'.")
+                print(f"You already guessed the word '{guess}'.")
             elif guess != word:
                 print(f"'{guess}' is not the word.")
                 tries -= 1
@@ -213,47 +227,50 @@ def play_game(word, max_tries, hint):
                 score += 20
 
         else:
-            print("Invalid input, Please guess a single letter or the entire word.")
+            print("Invalid input!Please, enter letter or the entire word.")
         """
         system offers a hint to help to guess the word
         """
-        if tries == max_tries  // 3 and hints_used == 0:
-            use_hint = input (f"Would you like a hint? (Y/N):").strip().lower()
+
+        if tries == max_tries // 3 and hints_used == 0:
+            use_hint = input(f"Would you like a hint? (Y/N):").strip().lower()
             if use_hint == 'y':
                 print(f"Hint: {hint}")
                 hints_used += 1
 
         print(display_hangman(max_tries-tries, max_tries))
         print(word_completion)
-        print(f"Tries left: {tries}")
+        print(f"Guesses left: {tries}")
         print("\n")
-
 
     if guessed:
         score += (tries * 6)  # Bonus points for remaining tries
-        print(f"Congratulations! You guessed the word '{word}'! Your score: {score}")
+        print(f"You guessed the word '{word}'!Your score: {score}")
     else:
-        print(f"Sorry, you ran out of tries. The word was '{word}'. Your score: {score}")
+        print(f"No more guesses left!The word was '{word}'.Keep trying! Your score: {score}")
+
 
 """
 Main function to start the game loop
 """
+
+
 def main():
     max_tries = 7
     while True:
         division = choose_division(divisions)
         word = choose_word(division)
-        hint = hints.get(word) #get the hint
+        hint = hints.get(word)  # get the hint
         play_game(word, max_tries, hint)
 
-        if input("Do you want to play again? (Y/N):").strip().upper() != 'Y':
-            print("Thank you for playing Hangman! Goodbye!")
+        if input("Would you like to play again? (Y/N):").strip().upper()!= 'Y':
+            print("Thank you for playing Hangman! Bye, bye!")
             break
+
+
 
 """
 Start the game
 """
 if __name__ == "__main__":
     main()
-
-        
